1eb880dce84d7269445754d3aa358187cdc40e51
k-fish <kevan.fisher@sentry.io>
2022-02-22 10:29:48 -0500

    feat(perf): Add anomalies backend and summary graphs
    This is an early PR for the anomalies frontend work, still behind a
    feature flag. This hooks up the actual anomalies backend and adds
    anomalies to the transaction summary tpm graph.

--- tests/js/spec/views/performance/transactionAnomalies/index.spec.tsx
+++ tests/js/spec/views/performance/transactionAnomalies/index.spec.tsx
@@ -24,6 +24,30 @@ const WrappedComponent = data => {
 };
 
 describe('AnomaliesTab', function () {
+  let anomaliesMock: any;
+
+  beforeEach(function () {
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/projects/',
+      body: [],
+    });
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/sdk-updates/',
+      body: [],
+    });
+    MockApiClient.addMockResponse({
+      url: '/prompts-activity/',
+      body: {},
+    });
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/events-has-measurements/',
+      body: {measurements: false},
+    });
+    anomaliesMock = MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/transaction-anomaly-detection/',
+      body: {},
+    });
+  });
   afterEach(cleanup);
   it('renders basic UI elements with flag', async function () {
     const initialData = initializeData({
@@ -39,5 +63,22 @@ describe('AnomaliesTab', function () {
     );
 
     expect(await screen.findByText('Transaction Count')).toBeInTheDocument();
+
+    expect(anomaliesMock).toHaveBeenCalledTimes(1);
+
+    expect(anomaliesMock).toHaveBeenNthCalledWith(
+      1,
+      expect.anything(),
+      expect.objectContaining({
+        query: expect.objectContaining({
+          environment: [],
+          field: ['tpm()'],
+          per_page: 50,
+          project: ['1'],
+          query: 'transaction:transaction',
+          statsPeriod: '14d',
+        }),
+      })
+    );
   });
 });
