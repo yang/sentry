2c7f9ef2e8c584acec4227aaff6edb76348c62e3
Stephen Cefali <scefali@sentry.io>
2022-08-22 10:44:18 -0700

    progress

---
+++ app/root.tsx
@@ -0,0 +1,26 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react';
+
+export default function App() {
+  return (
+    <html lang="en">
+      Hi
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
