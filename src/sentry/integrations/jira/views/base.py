886d160e362ea8683dc4d107fcaa8ad1bd18a498
Alexander Tarasov <alex.tarasov@sentry.io>
2023-05-05 16:18:22 +0200

    temporarily disable CSP due to middleware conflicts

--- src/sentry/integrations/jira/views/base.py
+++ src/sentry/integrations/jira/views/base.py
@@ -1,6 +1,6 @@
 from django.views.generic import View
 
-from sentry import options
+# from sentry import options
 from sentry.web.helpers import render_to_response
 
 
@@ -17,10 +17,10 @@ class JiraSentryUIBaseView(View):
 
         context["ac_js_src"] = "https://connect-cdn.atl-paas.net/all.js"
         response = render_to_response(self.html_file, context, self.request)
-        sources = [
-            self.request.GET.get("xdm_e"),
-            options.get("system.url-prefix"),
-        ]
-        sources_string = " ".join(s for s in sources if s)  # Filter out None
-        response["Content-Security-Policy"] = f"frame-ancestors 'self' {sources_string}"
+        # sources = [
+        #     self.request.GET.get("xdm_e"),
+        #     options.get("system.url-prefix"),
+        # ]
+        # sources_string = " ".join(s for s in sources if s)  # Filter out None
+        # response["Content-Security-Policy"] = f"frame-ancestors 'self' {sources_string}"
         return response
