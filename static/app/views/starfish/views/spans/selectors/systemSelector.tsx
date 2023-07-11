28c69d1994d6495911e08d1b7db08734adcae57a
Ash Anand <ash.anand@sentry.io>
2023-07-11 15:22:12 -0400

    Allow db.redis spans in DB module

--- static/app/views/starfish/views/spans/selectors/systemSelector.tsx
+++ static/app/views/starfish/views/spans/selectors/systemSelector.tsx
@@ -2,7 +2,6 @@ import {browserHistory} from 'react-router';
 
 import {CompactSelect} from 'sentry/components/compactSelect';
 import {t} from 'sentry/locale';
-
 import {useLocation} from 'sentry/utils/useLocation';
 
 export function SystemSelector() {
@@ -13,13 +12,6 @@ export function SystemSelector() {
 
   const location = useLocation();
   const value = location.query.system ?? SYSTEM_OPTIONS[0].value;
-  console.log(value);
-  // const eventView = getEventView(location, ModuleName.DB, spanCategory);
-
-  // const {data: operations} = useSpansQuery<[{'span.op': string}]>({
-  //   eventView,
-  //   initialData: [],
-  // });
 
   return (
     <CompactSelect
@@ -38,33 +30,3 @@ export function SystemSelector() {
     />
   );
 }
-
-// function getEventView(location: Location, moduleName: ModuleName, spanCategory?: string) {
-//   const queryConditions: string[] = [];
-//   if (moduleName) {
-//     queryConditions.push(`span.module:${moduleName}`);
-//   }
-
-//   if (moduleName === ModuleName.DB) {
-//     queryConditions.push('!span.op:db.redis');
-//   }
-
-//   if (spanCategory) {
-//     if (spanCategory === NULL_SPAN_CATEGORY) {
-//       queryConditions.push(`!has:span.category`);
-//     } else if (spanCategory !== 'Other') {
-//       queryConditions.push(`span.category:${spanCategory}`);
-//     }
-//   }
-//   return EventView.fromNewQueryWithLocation(
-//     {
-//       name: '',
-//       fields: ['span.op', 'count()'],
-//       orderby: '-count',
-//       query: queryConditions.join(' '),
-//       dataset: DiscoverDatasets.SPANS_METRICS,
-//       version: 2,
-//     },
-//     location
-//   );
-// }
