190920b3e3077c94b627529220f4e08e1a61c537
Nisanthan Nanthakumar <nnutoronto2013@gmail.com>
2023-08-09 00:41:23 -0400

    undo changes

--- static/app/views/settings/organizationGeneralSettings/organizationSettingsForm.tsx
+++ static/app/views/settings/organizationGeneralSettings/organizationSettingsForm.tsx
@@ -22,7 +22,6 @@ import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {Organization, OrganizationAuthProvider, Scope} from 'sentry/types';
 import withOrganization from 'sentry/utils/withOrganization';
-import {JsonFormObject} from 'sentry/components/forms/types';
 
 const HookCodecovSettingsLink = HookOrDefault({
   hookName: 'component:codecov-integration-settings-link',
@@ -38,21 +37,17 @@ interface Props extends RouteComponentProps<{}, {}> {
 
 interface State extends AsyncComponentState {
   authProvider: OrganizationAuthProvider;
-  featureFlags: {[key: string]: {value: boolean; description: string}};
 }
 
 class OrganizationSettingsForm extends DeprecatedAsyncComponent<Props, State> {
   getEndpoints(): ReturnType<DeprecatedAsyncComponent['getEndpoints']> {
     const {organization} = this.props;
-    return [
-      ['authProvider', `/organizations/${organization.slug}/auth-provider/`],
-      ['featureFlags', '/internal/feature-flags/'],
-    ];
+    return [['authProvider', `/organizations/${organization.slug}/auth-provider/`]];
   }
 
   render() {
     const {initialData, organization, onSave, access} = this.props;
-    const {authProvider, featureFlags} = this.state;
+    const {authProvider} = this.state;
     const endpoint = `/organizations/${organization.slug}/`;
 
     const jsonFormSettings = {
@@ -109,42 +104,32 @@ class OrganizationSettingsForm extends DeprecatedAsyncComponent<Props, State> {
       },
     ];
 
-    const featuresForm: JsonFormObject = {
-      title: t('Early Adopter Features'),
-      fields: Object.entries(featureFlags || {}).map(([flag, obj]) => ({
-        label: obj.description,
-        name: flag,
-        type: 'boolean',
-      })),
-    };
     return (
-      <>
-        <Form
-          data-test-id="organization-settings"
-          apiMethod="PUT"
-          apiEndpoint={endpoint}
-          saveOnBlur
-          allowUndo
-          initialData={initialData}
-          onSubmitSuccess={(updated, _model) => {
-            // Special case for slug, need to forward to new slug
-            if (typeof onSave === 'function') {
-              onSave(initialData, updated);
-            }
-          }}
-          onSubmitError={() => addErrorMessage('Unable to save change')}
-        >
-          <JsonForm {...jsonFormSettings} forms={forms} />
-          <AvatarChooser
-            type="organization"
-            allowGravatar={false}
-            endpoint={`${endpoint}avatar/`}
-            model={initialData}
-            onSave={updateOrganization}
-            disabled={!access.has('org:write')}
-          />
-        </Form>
-      </>
+      <Form
+        data-test-id="organization-settings"
+        apiMethod="PUT"
+        apiEndpoint={endpoint}
+        saveOnBlur
+        allowUndo
+        initialData={initialData}
+        onSubmitSuccess={(updated, _model) => {
+          // Special case for slug, need to forward to new slug
+          if (typeof onSave === 'function') {
+            onSave(initialData, updated);
+          }
+        }}
+        onSubmitError={() => addErrorMessage('Unable to save change')}
+      >
+        <JsonForm {...jsonFormSettings} forms={forms} />
+        <AvatarChooser
+          type="organization"
+          allowGravatar={false}
+          endpoint={`${endpoint}avatar/`}
+          model={initialData}
+          onSave={updateOrganization}
+          disabled={!access.has('org:write')}
+        />
+      </Form>
     );
   }
 }
